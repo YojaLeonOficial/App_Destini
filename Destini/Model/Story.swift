7b83da92c96e5f435cdaa36cf5f90f238f703820
YojaLeonOficial <yojairoleonoficial@gmail.com>
2023-02-10 14:40:05 +0100

    Initial Commit

---
+++ Destini/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Yojairo Jose Leon Escobar on 28/12/22.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
